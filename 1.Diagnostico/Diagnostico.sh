77c3c064b2c3f3905478a008a3ccc0e9bc28c000
Julio Lugo <juliocesarfy6@gmail.com>
2026-02-12 17:38:15 -0700

    diagnostico correccion

--- 1.Diagnostico/Diagnostico.sh
+++ 1.Diagnostico/Diagnostico.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-# Script de diagnóstico inicial - Redes y Sistemas
 echo "=========================================="
 echo "   DIAGNÓSTICO DE NODO LINUX"
 echo "=========================================="
 echo "Hostname:      $(hostname)"
-# Extrae la IP de la red interna (enp0s8)
 echo "IP Interna:    $(ip addr show enp0s8 | grep 'inet ' | awk '{print $2}')"
 echo "Espacio Disco: $(df -h / | tail -1 | awk '{print $4}') disponible"
 echo "=========================================="
